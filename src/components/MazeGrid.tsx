4f6224a4755f1b20fa719375927ab7a70e1df0b6
hashtoes <hashtoes@gmail.com>
2025-06-03 07:19:31 +0900

    fixed player

--- src/components/MazeGrid.tsx
+++ src/components/MazeGrid.tsx
@@ -6,29 +6,44 @@ import './MazeGrid.css';
 interface MazeGridProps {
     maze: Cell[][];
 }
-
 const MazeGrid: React.FC<MazeGridProps> = ({ maze }) => {
+    const height = maze.length;
+
     return (
         <div className="maze-grid">
-            {[...maze].reverse().map((row, rowIndex) => (
-                <div className="maze-row" key={maze.length - 1 - rowIndex}>
-                    {row.map((cell, colIndex) => {
-                        const style = {
-                            borderTop: cell.top ? '2px solid #333' : '2px solid transparent',
-                            borderRight: cell.right ? '2px solid #333' : '2px solid transparent',
-                            borderBottom: cell.bottom ? '2px solid #333' : '2px solid transparent',
-                            borderLeft: cell.left ? '2px solid #333' : '2px solid transparent',
-                        };
-                        return (
-                            <div
-                                key={`${maze.length - 1 - rowIndex}-${colIndex}`}
-                                className="maze-cell"
-                                style={style}
-                            />
-                        );
-                    })}
-                </div>
-            ))}
+            {[...maze].reverse().map((row, rowIndexReversed) => {
+                const y = height - 1 - rowIndexReversed;
+                return (
+                    <div className="maze-row" key={y}>
+                        {row.map((cell, x) => {
+                            const style = {
+                                borderTop: cell.top ? '2px solid #333' : '2px solid transparent',
+                                borderRight: cell.right ? '2px solid #333' : '2px solid transparent',
+                                borderBottom: cell.bottom ? '2px solid #333' : '2px solid transparent',
+                                borderLeft: cell.left ? '2px solid #333' : '2px solid transparent',
+                            };
+
+                            const isPlayer = x === 0 && y === 0;
+
+                            return (
+                                <div
+                                    key={`${x}-${y}`}
+                                    className="maze-cell"
+                                    style={{
+                                        ...style,
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    {isPlayer ? 'x' : ''}
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
         </div>
     );
 };
